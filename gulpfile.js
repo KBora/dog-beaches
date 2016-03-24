51da9b3673053f474398c39a874a15677a7bb488
Kian Woo <kian.woo@gmail.com>
2016-03-25 08:14:01 +1100

    Using SASS via gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('default', [
-	'copy-html','copy-css', 'copy-js'], function() {
+	'copy-html','copy-css', 'copy-js', 'copy-js-lib'], function() {
 	// place code for your default task here
-		browserSync.init({
-			server: './dist'
-		});
+
 });
 
 gulp.task('copy-html', function() {
@@ -18,7 +17,11 @@ gulp.task('copy-html', function() {
 
 
 gulp.task('copy-css', function() {
-	gulp.src('./src/css/*.css' )
+	gulp.src('./src/sass/*.scss' )
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
 		.pipe(gulp.dest('./dist/css'));
 });
 
@@ -26,4 +29,9 @@ gulp.task('copy-js', function() {
 	gulp.src('./src/js/*.js' )
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('copy-js-lib', function() {
+	gulp.src('./src/js/lib/*.js' )
+		.pipe(gulp.dest('./dist/js/lib'));
 });
