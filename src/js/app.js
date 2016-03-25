2122f891f7901d7a3b8bcebb539a8853c084b69c
Kian Woo <kian.woo@gmail.com>
2016-03-25 18:04:00 +1100

    Fix indenting in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,69 @@
-var dogBeachInitialData = 
-    [{   title : 'Sirius Cove',
-        position : {lat: -33.839502, lng: 151.237244},
-        offLeash: true,
-        offLeashTimes : 'All day Monday to Friday, and before 9am and after 4pm on Saturdays, Sundays and Public Holidays ',
-        website : 'http://www.mosman.nsw.gov.au/residents/Pets/where-to-walk-your-dog/'
-    },
-    {   title : 'Clifton Gardens',
-        position : {lat: -33.838516, lng: 151.254275},
-        offLeash : true,
-        offLeashTimes: 'Before 9am and after 4pm weekdays during winter. Before 9am and after 6pm weekdays during daylight savings summer months (Oct – Mar)',
-        website : 'http://www.mosman.nsw.gov.au/residents/Pets/where-to-walk-your-dog/'
-    },
-    {   title : 'Dumaresq Reserve',
-        position : {lat: -33.866418, lng: 151.270572},
-        offLeash : true,
-        offLeashTimes: 'Between 4.30pm and 8.30am',
-        website : 'http://www.woollahra.nsw.gov.au/services/animals_and_pets/walking_your_dog'
-    },
-    {   title : 'Rowland Reserve',
-        position : {lat: -33.660741, lng: 151.305761},
-        offLeash : true,
-        offLeashTimes: 'All times',
-        website : 'http://www.pittwater.nsw.gov.au/property/domestic_animals/dogs/exercise_areas'
-    },
-    {   title : 'Flora and Richie Roberts Reserve',
-        position : {lat: -33.766673, lng: 151.298436},
-        offLeash : true,
-        offLeashTimes: 'All times',
-        website : 'http://www.warringah.nsw.gov.au/play/flora-and-ritchie-roberts-reserve'
-    },
-    {   title : 'Bonna Point Reserve',
-		    position : {lat: -34.007235, lng: 151.190262},
-		    offLeash : true,
-		    offLeashTimes: 'All times',
-		    website : 'http://www.sutherlandshire.nsw.gov.au/Outdoors/Parks-and-Playgrounds/Parks/Bonna-Point-Reserve-Kurnell'
-    },
-    {   title : 'Bondi to Bronte coastal walk',
-		    position : {lat: -33.895492, lng: 151.274602},
-		    offLeash : false,
-		    offLeashTimes: 'Dogs must be on a leash',
-		    website : 'http://www.waverley.nsw.gov.au/recreation/beaches_and_coast/bondi_to_bronte_coastal_walk'
-    }
+var dogBeachInitialData = [{
+	title: 'Sirius Cove',
+	position: {
+		lat: -33.839502,
+		lng: 151.237244
+	},
+	offLeash: true,
+	offLeashTimes: 'All day Monday to Friday, and before 9am and after 4pm on Saturdays, Sundays and Public Holidays ',
+	website: 'http://www.mosman.nsw.gov.au/residents/Pets/where-to-walk-your-dog/'
+}, {
+	title: 'Clifton Gardens',
+	position: {
+		lat: -33.838516,
+		lng: 151.254275
+	},
+	offLeash: true,
+	offLeashTimes: 'Before 9am and after 4pm weekdays during winter. Before 9am and after 6pm weekdays during daylight savings summer months (Oct – Mar)',
+	website: 'http://www.mosman.nsw.gov.au/residents/Pets/where-to-walk-your-dog/'
+}, {
+	title: 'Dumaresq Reserve',
+	position: {
+		lat: -33.866418,
+		lng: 151.270572
+	},
+	offLeash: true,
+	offLeashTimes: 'Between 4.30pm and 8.30am',
+	website: 'http://www.woollahra.nsw.gov.au/services/animals_and_pets/walking_your_dog'
+}, {
+	title: 'Rowland Reserve',
+	position: {
+		lat: -33.660741,
+		lng: 151.305761
+	},
+	offLeash: true,
+	offLeashTimes: 'All times',
+	website: 'http://www.pittwater.nsw.gov.au/property/domestic_animals/dogs/exercise_areas'
+}, {
+	title: 'Flora and Richie Roberts Reserve',
+	position: {
+		lat: -33.766673,
+		lng: 151.298436
+	},
+	offLeash: true,
+	offLeashTimes: 'All times',
+	website: 'http://www.warringah.nsw.gov.au/play/flora-and-ritchie-roberts-reserve'
+}, {
+	title: 'Bonna Point Reserve',
+	position: {
+		lat: -34.007235,
+		lng: 151.190262
+	},
+	offLeash: true,
+	offLeashTimes: 'All times',
+	website: 'http://www.sutherlandshire.nsw.gov.au/Outdoors/Parks-and-Playgrounds/Parks/Bonna-Point-Reserve-Kurnell'
+}, {
+	title: 'Bondi to Bronte coastal walk',
+	position: {
+		lat: -33.895492,
+		lng: 151.274602
+	},
+	offLeash: false,
+	offLeashTimes: 'Dogs must be on a leash',
+	website: 'http://www.waverley.nsw.gov.au/recreation/beaches_and_coast/bondi_to_bronte_coastal_walk'
+}
 
-    ];
+];
 
 
 var DogBeachMarker = function(data) {
